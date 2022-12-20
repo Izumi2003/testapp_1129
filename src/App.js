509e6a4e446eea4a53fddce95c3e0198b5bc2baa
izumi <smsoihi2703@gmail.com>
2022-12-20 11:46:24 +0900

    変更

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import './App.css';
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  // //カウントを保存するための、カウント、セットカウント（⇡変更するための）を用意する
-  // const [count,setCount] = useState(0);
+  return (<Weather></Weather>)
+};
+// //カウントを保存するための、カウント、セットカウント（⇡変更するための）を用意する
+// const [count,setCount] = useState(0);
 
-  // //useEffectは、クリックした回数を⤵⤵⤵
-  // //document.titleで、タイトルタグの部分に表示させる（書き変わっている）（ドキュメントのタイトル部分）
-  // useEffect(() => {
-  //   document.title = `You clicked ${count} times`;
-  // });
+// //useEffectは、クリックした回数を⤵⤵⤵
+// //document.titleで、タイトルタグの部分に表示させる（書き変わっている）（ドキュメントのタイトル部分）
+// useEffect(() => {
+//   document.title = `You clicked ${count} times`;
+// });
 
-  // return (
-  //   <>
-  //     <p>あなたは{count}回クリックしました！</p>
-  //     <button onClick={() => setCount(count + 1)}>Click</button>
-  //     <button onClick={function () {
-  //       return setCount(count + 1)
-  //     }}>Click</button>
-  //   </>
-  // );
-  //functionバージョン↑
+// return (
+//   <>
+//     <p>あなたは{count}回クリックしました！</p>
+//     <button onClick={() => setCount(count + 1)}>Click</button>
+//     <button onClick={function () {
+//       return setCount(count + 1)
+//     }}>Click</button>
+//   </>
+// );
+//functionバージョン↑
 
 //   const handleClick = () => {
 //     //クリックされた時の処理
@@ -31,20 +33,44 @@ function App() {
 // const button = <button onClick={handleClick}>クリックしてね</button>;
 
 
-  // const [addrtype, _] = useState(["東京", "大阪", "札幌"])
-  // const [value, setValue] = useState("");
+// const [addrtype, _] = useState(["東京", "大阪", "札幌"])
+// const [value, setValue] = useState("");
 
-  // useEffect(() => {
-  //   document.alert = `You clicked ${Add} times`;
-  // });
+// useEffect(() => {
+//   document.alert = `You clicked ${Add} times`;
+// });
+
+const Weather = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   function handleChange(a) {
     alert(a);
   }
-  //ハンドルチェンジされたときにqueryWeatherが実行される
+  async function querWether() {
+    const url =
+      "https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json";
+    const response = await fetch(url);
+    const jsondata = await response.json();
+    setData(jsondata[0].timeSeries[0].areas[0]);
+    setLoading(false);
+  }
+  useEffect(() => {
+    querWether();
+  }, []);
+  let wetherinfo;
+  if (loading) {
+    wetherinfo = <p>loading</p>;
+  } else {
+    wetherinfo = (
+      <p>{data.area.name}の明日の天気は{data.weathers[0]}</p>
+    )
+  }
   return (
     <>
       <h1>Wether</h1>
       <p>今日の天気は</p>
+      {wetherinfo}
       <select onChange={e => handleChange(e.target.value)}>
         <option value="tokyo">東京</option>
         <option value="osaka">大阪</option>
@@ -52,6 +78,13 @@ function App() {
       </select>
     </>
   )
-  }
+}
+
+
+
+
+//ハンドルチェンジされたときにqueryWeatherが実行される
+
+
 
 export default App;
